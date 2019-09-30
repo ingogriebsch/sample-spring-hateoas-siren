1c31f858d932eabf8bed9374ca7f41fd86781c1d
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-09-30 22:46:28 +0200

    test: rework whole test class

--- src/test/java/org/springframework/hateoas/mediatype/siren/Jackson2SirenModuleTest.java
+++ src/test/java/org/springframework/hateoas/mediatype/siren/Jackson2SirenModuleTest.java
@@ -19,8 +19,12 @@
  */
 package org.springframework.hateoas.mediatype.siren;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.hateoas.IanaLinkRelations.ABOUT;
+import static org.springframework.hateoas.IanaLinkRelations.HELP;
+import static org.springframework.hateoas.IanaLinkRelations.LICENSE;
 import static org.springframework.hateoas.IanaLinkRelations.SELF;
 
 import java.io.StringWriter;
@@ -33,6 +37,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
@@ -42,6 +47,20 @@ import org.springframework.hateoas.server.core.DefaultLinkRelationProvider;
 import org.springframework.hateoas.server.core.DelegatingLinkRelationProvider;
 
 public class Jackson2SirenModuleTest {
+
+    private static final String REPRESENTATION_MODEL_WITHOUT_LINKS = "{}";
+    private static final String REPRESENTATION_MODEL_CONTAINING_LINK = "{\"links\":[{\"rel\":[\"about\"],\"href\":\"/about\"}]}";
+    private static final String REPRESENTATION_MODEL_CONTAINING_LINKS =
+        "{\"links\":[{\"rel\":[\"about\"],\"href\":\"/about\"},{\"rel\":[\"help\"],\"href\":\"/help\"},{\"rel\":[\"license\"],\"href\":\"/license\"}]}";
+    private static final String ENTITY_MODEL_CONTAINING_POJO =
+        "{\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42}}";
+    private static final String ENTITY_MODEL_CONTAINING_POJO_AND_SELF_LINK =
+        "{\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42},\"links\":[{\"rel\":[\"self\"],\"href\":\"/persons/1\"}]}";
+    private static final String COLLECTION_MODEL_WITHOUT_CONTENT = "{\"class\":[\"collection\"],\"properties\":{\"size\":0}}";
+    private static final String COLLECTION_MODEL_CONTAINING_ENTITY_MODEL_CONTAINING_POJO =
+        "{\"class\":[\"collection\"],\"properties\":{\"size\":1},\"entities\":[{\"rel\":[\"item\"],\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42}}]}";
+    private static final String COLLECTION_MODEL_CONTAINING_ENTITY_MODEL_CONTAINING_POJO_AND_SELF_LINK =
+        "{\"class\":[\"collection\"],\"properties\":{\"size\":1},\"entities\":[{\"rel\":[\"item\"],\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42},links:[{\"rel\":[\"self\"],\"href\":\"/persons/1\"}]}";
 
     private static ObjectMapper objectMapper;
 
@@ -61,9 +80,28 @@ public class Jackson2SirenModuleTest {
         class Representation {
 
             @Test
-            public void should_return_matching_link() throws Exception {
+            public void without_links() throws Exception {
+                RepresentationModel<?> source = new RepresentationModel<>();
+                String expected = REPRESENTATION_MODEL_WITHOUT_LINKS;
+
+                String actual = write(source);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_link() throws Exception {
                 RepresentationModel<?> source = new RepresentationModel<>(new Link("/about", ABOUT));
-                String expected = "{\"links\":[{\"rel\":[\"about\"],\"href\":\"/about\"}]}";
+                String expected = REPRESENTATION_MODEL_CONTAINING_LINK;
+
+                String actual = write(source);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_links() throws Exception {
+                RepresentationModel<?> source = new RepresentationModel<>(
+                    newArrayList(new Link("/about", ABOUT), new Link("/help", HELP), new Link("/license", LICENSE)));
+                String expected = REPRESENTATION_MODEL_CONTAINING_LINKS;
 
                 String actual = write(source);
                 assertThat(actual).isEqualTo(expected);
@@ -74,25 +112,55 @@ public class Jackson2SirenModuleTest {
         class Entity {
 
             @Test
-            public void should_return_matching_class_and_properties() throws Exception {
+            public void containing_pojo() throws Exception {
                 EntityModel<Person> source = new EntityModel<>(new Person("Peter", 42));
-                String expected = "{\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42}}";
+                String expected = ENTITY_MODEL_CONTAINING_POJO;
 
                 String actual = write(source);
                 assertThat(actual).isEqualTo(expected);
             }
 
             @Test
-            public void should_return_matching_class_and_properties_and_self_link() throws Exception {
+            public void containing_pojo_and_self_link() throws Exception {
                 EntityModel<Person> source = new EntityModel<>(new Person("Peter", 42), new Link("/persons/1", SELF));
-                String expected =
-                    "{\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42},\"links\":[{\"rel\":[\"self\"],\"href\":\"/persons/1\"}]}";
+                String expected = ENTITY_MODEL_CONTAINING_POJO_AND_SELF_LINK;
 
                 String actual = write(source);
                 assertThat(actual).isEqualTo(expected);
             }
         }
 
+        @Nested
+        class Collection {
+
+            @Test
+            public void without_content() throws Exception {
+                CollectionModel<?> source = new CollectionModel<>(newArrayList());
+                String expected = COLLECTION_MODEL_WITHOUT_CONTENT;
+
+                String actual = write(source);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_entity_model_containing_pojo() throws Exception {
+                CollectionModel<?> source = new CollectionModel<>(newArrayList(new EntityModel<>(new Person("Peter", 42))));
+                String expected = COLLECTION_MODEL_CONTAINING_ENTITY_MODEL_CONTAINING_POJO;
+
+                String actual = write(source);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_entity_model_containing_pojo_and_self_link() throws Exception {
+                CollectionModel<?> source =
+                    new CollectionModel<>(newArrayList(new EntityModel<>(new Person("Peter", 42), new Link("/persons/1", SELF))));
+                String expected = COLLECTION_MODEL_CONTAINING_ENTITY_MODEL_CONTAINING_POJO_AND_SELF_LINK;
+
+                String actual = write(source);
+                assertThat(actual).isEqualTo(expected);
+            }
+        }
     }
 
     @Nested
@@ -103,9 +171,28 @@ public class Jackson2SirenModuleTest {
         class Representation {
 
             @Test
-            public void should_return_representation_model_containing_link() throws Exception {
-                String source = "{\"link\":[\"about\"],\"href\":\"/about\"}";
+            public void without_links() throws Exception {
+                String source = REPRESENTATION_MODEL_WITHOUT_LINKS;
+                RepresentationModel<?> expected = new RepresentationModel<>();
+
+                RepresentationModel<?> actual = read(source, RepresentationModel.class);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_link() throws Exception {
+                String source = REPRESENTATION_MODEL_CONTAINING_LINK;
                 RepresentationModel<?> expected = new RepresentationModel<>(new Link("/about", ABOUT));
+
+                RepresentationModel<?> actual = read(source, RepresentationModel.class);
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_links() throws Exception {
+                String source = REPRESENTATION_MODEL_CONTAINING_LINKS;
+                RepresentationModel<?> expected = new RepresentationModel<>(
+                    newArrayList(new Link("/about", ABOUT), new Link("/help", HELP), new Link("/license", LICENSE)));
 
                 RepresentationModel<?> actual = read(source, RepresentationModel.class);
                 assertThat(actual).isEqualTo(expected);
@@ -116,13 +203,42 @@ public class Jackson2SirenModuleTest {
         class Entity {
 
             @Test
-            public void should_return_matching_pojo() throws Exception {
-                String source = "{\"class\":[\"person\"],\"properties\":{\"name\":\"Peter\",\"age\":42}}";
+            public void containing_pojo() throws Exception {
+                String source = ENTITY_MODEL_CONTAINING_POJO;
                 JavaType expectedType = objectMapper.getTypeFactory().constructParametricType(EntityModel.class, Person.class);
                 EntityModel<Person> expected = new EntityModel<>(new Person("Peter", 42));
 
                 EntityModel<Person> actual = read(source, expectedType);
                 assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            public void containing_pojo_and_self_link() throws Exception {
+                String source = ENTITY_MODEL_CONTAINING_POJO_AND_SELF_LINK;
+                JavaType expectedType = objectMapper.getTypeFactory().constructParametricType(EntityModel.class, Person.class);
+                EntityModel<Person> expected = new EntityModel<>(new Person("Peter", 42), new Link("/persons/1", SELF));
+
+                EntityModel<Person> actual = read(source, expectedType);
+                assertThat(actual).isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        class Collection {
+
+            @Test
+            public void without_content() throws Exception {
+                fail("Implement me... :)");
+            }
+
+            @Test
+            public void containing_entity_model_containing_pojo() throws Exception {
+                fail("Implement me... :)");
+            }
+
+            @Test
+            public void containing_entity_model_containing_pojo_and_self_link() throws Exception {
+                fail("Implement me... :)");
             }
         }
     }
