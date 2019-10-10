6b45b3f88092b7e993a7ae8907ae7d244fcc927f
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-10-10 15:57:07 +0200

    test: introduce WebMvcIntegrationTest and dependent

--- src/test/java/org/springframework/hateoas/support/MappingUtils.java
+++ src/test/java/org/springframework/hateoas/support/MappingUtils.java
@@ -1,9 +1,12 @@
 /*
  * Copyright 2015-2017 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,29 +25,29 @@ import org.springframework.core.io.Resource;
  */
 public final class MappingUtils {
 
-    /**
-     * Read test files.
-     *
-     * @param resource as a {@link Resource}
-     * @return
-     * @throws IOException
-     */
-    public static String read(Resource resource) throws IOException {
+	/**
+	 * Read test files.
+	 *
+	 * @param resource as a {@link Resource}
+	 * @return
+	 * @throws IOException
+	 */
+	public static String read(Resource resource) throws IOException {
 
-        try (Scanner scanner = new Scanner(resource.getInputStream())) {
+		try (Scanner scanner = new Scanner(resource.getInputStream())) {
 
-            StringBuilder builder = new StringBuilder();
+			StringBuilder builder = new StringBuilder();
 
-            while (scanner.hasNextLine()) {
+			while (scanner.hasNextLine()) {
 
-                builder.append(scanner.nextLine());
+				builder.append(scanner.nextLine());
 
-                if (scanner.hasNextLine()) {
-                    builder.append(System.lineSeparator());
-                }
-            }
+				if (scanner.hasNextLine()) {
+					builder.append(System.lineSeparator());
+				}
+			}
 
-            return builder.toString();
-        }
-    }
+			return builder.toString();
+		}
+	}
 }
